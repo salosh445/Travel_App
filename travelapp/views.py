532fa671850c386645ddcde60e004ae3b67aa306
unknown <saloshkumar445@gmail.com>
2022-12-17 10:37:57 +0530

    first

---
+++ travelapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from . models import place
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def fun(request):
+    obj=place.objects.all()
+
+
+    return render(request,"index.html",{'results':obj})
+
+
+def addition(request):
+    num1=int(request.POST["num1"])
+    num2=int(request.POST["num2"])
+    num3=num1+num2
+    return render(request,"result.html",{"add":num3})
